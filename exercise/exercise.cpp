1426d53505c7602b58ff0b09db119b00e9ffc7b4
Hikmat Farhat <hfarhat@ndu.edu.lb>
2020-04-16 02:59:15 +0300

    shoud add the output to the issue

--- exercise/exercise.cpp
+++ exercise/exercise.cpp
@@ -15,13 +15,12 @@ int main()
 	l.push_back("eight");
 	l.push_back("one");
 	l.push_back("nine");
-	std::cout<<"Your output is \n";
+	std::cout<<"**Your output is** \n\n";
 	for (auto x : l) {
-		std::cout << x << "|";
+		std::cout << x << " | ";
 	}
 	std::cout << std::endl;
-	std::cout<<"IT SHOULD BE \n";
+	std::cout<<"\n\n**IT SHOULD BE** \n\n";
 	std::cout<<"1 2 3 4 5 6\n";
-	return 1;
 	
 }
