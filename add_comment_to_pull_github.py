2fe364a74abfade8d4c691c5f0eda84552accebf
Hikmat Farhat <hfarhat@ndu.edu.lb>
2020-04-16 02:34:38 +0300

    added add_comment_to_pull_github.py

---
+++ add_comment_to_pull_github.py
@@ -0,0 +1,9 @@
+from github import Github
+g = Github("a3416810202a7eb806e9cd1dd788bc675fa04d3f")
+remote_repo_name="csc213-inclass-STL"
+
+github_repo=g.get_user().get_repo(remote_repo_name)
+issue=github_repo.get_issue(12)
+f=open("example-content/output.md")
+comment=f.read()
+issue.create_comment(comment)
