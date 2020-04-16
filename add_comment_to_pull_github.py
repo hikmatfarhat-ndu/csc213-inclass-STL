14193064d1b833e580e3d9d40abe6988ed609106
Hikmat Farhat <hfarhat@ndu.edu.lb>
2020-04-16 03:44:49 +0300

    some phoenician

--- add_comment_to_pull_github.py
+++ add_comment_to_pull_github.py
@@ -6,7 +6,7 @@ github_repo=g.get_user().get_repo(remote_repo_name)
 issue=github_repo.get_issue(12)
 f=open("example-content/output.md")
 comment=f.read()
-comment="&#67840;&#67841;&#67842;&#67843;&#67844;&#67845;"
+comment="&#67840;&#67841;&#67842;&#67843;&#67844;&#67845;"+comment
 comment="&#10060;\n"+comment
 comment=comment+"&#10004;\n"
 issue.create_comment(comment)
