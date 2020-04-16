03a1a36755c10c2ba6f0933c4ffcb3643fb7db98
Hikmat Farhat <hfarhat@ndu.edu.lb>
2020-04-16 03:12:41 +0300

    some color

--- add_comment_to_pull_github.py
+++ add_comment_to_pull_github.py
@@ -6,4 +6,6 @@ github_repo=g.get_user().get_repo(remote_repo_name)
 issue=github_repo.get_issue(12)
 f=open("example-content/output.md")
 comment=f.read()
+comment="&#10060;\n"+comment
+comment=comment+"&#9989;\n"
 issue.create_comment(comment)
